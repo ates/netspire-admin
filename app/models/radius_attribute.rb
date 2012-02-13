58c7ceb6af64a635c7882ac46345f966d752df5e
Artem Teslenko <ates@ipv6.dp.ua>
2012-02-13 14:09:29 +0200

    Remove radius replies from service link if attribute was deleted

--- app/models/radius_attribute.rb
+++ app/models/radius_attribute.rb
@@ -10,4 +10,15 @@ class RadiusAttribute
   timestamps!
 
   validates :name, :presence => true
+
+  before_destroy :remove_from_service_links
+
+  private
+
+  def remove_from_service_links
+    ServiceLink.all.each do |link|
+      link.radius_replies.delete_if {|reply| reply.name == self.name}
+      link.save!
+    end
+  end
 end
