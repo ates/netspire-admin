33c7b4a6040ba45365a153434c41f57cdd3f055f
Artem Teslenko <ates@ipv6.dp.ua>
2012-02-16 01:53:16 +0200

    Cosmetic changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+  def localized_time(value, format = :short, blank = nil)
+    value.present? ? l(value, :format => format) : blank
+  end
+
   def tick_or_cross(value)
     icon = value ? "ok" : "remove"
     "<i class='icon-#{icon}'></i>".html_safe
